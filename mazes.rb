5bd2de38c621652fad98250ea91473b1823ede0d
Ken Harsch <kharsch@brandeis.edu>
2014-02-10 19:51:52 -0500

    solve works

--- mazes.rb
+++ mazes.rb
@@ -2,6 +2,8 @@
 #Ken Harsch
 #kharsch@brandeis.edu
 
+
+
 class Maze
 
 	attr_reader :array, :n, :m, :maze, :check_values
@@ -42,33 +44,105 @@ class Maze
 		return @maze[x][y]
 	end
 
-	def check(a, b)
+	def check(x, y)
 		check_values = []
-		check_values[0] = get_value(a - 1, b)
-		check_values[1] = get_value(a + 1, b)
-		check_values[2] = get_value(a, b - 1)
-		check_values[3] = get_value(a, b + 1)
+		check_values[0] = get_value(x - 1, y)
+		check_values[1] = get_value(x + 1, y)
+		check_values[2] = get_value(x, y - 1)
+		check_values[3] = get_value(x, y + 1)
 		return check_values
 	end
 
 	def solve(begX, begY, endX, endY)
+		start = [begX, begY]
+		finish = [endX, endY]
+		stack = []
+		move_trace = []
+		dead = []
 		if get_value(begX, begY) == 1 || get_value(endX, endY) == 1
-			puts "Invalid starting or ending point"
-			return
+			message(1)
+			return false
 		end
-		trace = []
-		last  = (begX, begY)
-		can_move = true
-		while can_move == true
-			
-
+		stack.push(start)
+		move_trace.push(start)
+		while !stack.empty?
+			current = stack.pop()
+			puts "current stack pop = "
+			p current
+			puts "aft curr"
+			if current == finish
+				message(3)
+				return true
+			end		
+			trace(current).each do |node|
+				puts "node in curr loop ="
+				p node
+				if  !move_trace.include? node
+					stack.push(node)
+					move_trace.push(node)
+					puts "stack after push= "
+					p stack
+					puts "pushed load value = "	
+					p node
+				end
+			end
 		end
-
-
+		puts "end while loop"
+		message(4)
+		return false
 	end
 
-	def trace(begX, begY, endX, endY)
+	def message(m)
+		case m 
+		when 1
+			puts "Invalid starting or ending point"
+		when 2
+			puts "Cannot move from this position"
+		when 3
+			puts "Finished"
+		when 4
+			puts "Cannot be solved"
+		end	
+	end
 
+	def trace(node)
+		x = node[0]
+		y = node[1]
+
+		open_moves = []
+		values = check(x, y)
+		puts "values check in trace ="
+		p values
+		if values[0] == "0"
+			puts "v 0 ="
+			p values[0]
+			temp = [node[0] - 1, node[1]]
+			open_moves.push temp
+		end
+
+		if values[1] == "0"
+			puts "v 1 ="
+			p values[1]
+			temp = [node[0] + 1, node[1]]
+			open_moves.push temp
+		end
+
+		if values[2] == "0"
+			puts "v 2 ="
+			p values[2]
+			temp = [node[0], node[1] - 1]
+			open_moves.push temp
+		end
+
+		if values[3] == "0"
+			puts "v 3 ="
+			p values[3]
+			temp = [node[0], node[1] + 1]
+			open_moves.push temp
+		end
+		puts "open moves ="
+		p open_moves
+		return open_moves
 	end
 
 end
--- test_area.rb
+++ test_area.rb
@@ -4,9 +4,13 @@
 
 require_relative "mazes.rb"
 
+
+
 m = Maze.new(4,4)
 m.load("111111111101000001101011101101010101100010101111110101110000001110111111111111111")
 m.make_maze
 m.show_maze
-p m.get_value(2, 2)
-p m.check(2, 2)
+#p m.get_value(2, 6)
+puts "m check 44 = "
+p m.check(4, 3)
+m.solve(4, 3, 6, 4)
